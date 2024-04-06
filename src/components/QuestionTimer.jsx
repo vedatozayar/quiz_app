165e029a0d742938254d7a64d37d091ff29bb3b4
vedat <vedatozayar@gmail.com>
2024-04-07 02:29:15 +0300

    added timer

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,21 @@
+import { useState,useEffect } from "react";
+
+export default function QuestionTimer({timeout,onTimeout}) {
+    const [remainingTime,setRemainingTime]=useState(timeout)
+
+    useEffect(()=>{
+        setTimeout(onTimeout,timeout);
+    },[onTimeout,timeout])
+    
+
+    useEffect(()=>{
+        setInterval(()=>{
+            setRemainingTime((prevRemainingTime)=>prevRemainingTime-100)
+        },100)
+    
+    },[])
+
+   
+
+    return <progress id='question-time' max={timeout} value={remainingTime} />
+};
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
-import Questions from "../questions.js";
+import QUESTIONS from "../questions.js";
+import quizComplateImg from "../../src/assets/quiz-logo.png";
+import QuestionTimer from "./QuestionTimer.jsx";
+
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  
+  
+  const quizIsComplate= activeQuestionIndex===QUESTIONS.length
+  
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+  
+  if(quizIsComplate){
+    return (<div id="summary">
+      <img src={quizComplateImg} alt="Trophy Icon" />
+      <h2>Quiz Complated!</h2>
+    </div>)
+  }
+  const shuffledAnswers=[...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(()=>Math.random()-0.5);
+
+
   return (
     <div id="quiz">
       <div id="question">
-        <h2>{Questions[activeQuestionIndex].text}</h2>
+        <QuestionTimer timeout={10000} onTimeout={()=>handleSelectAnswer(null)} />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {Questions[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li className="answer" key={answer}>
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
